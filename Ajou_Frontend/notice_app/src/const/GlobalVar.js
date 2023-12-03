bc41ec0c9506cf588c0704a898536829030caa01
jiyseo <jji5602@gmail.com>
2023-12-04 04:46:10 +0900

    updata api url

--- Ajou_Frontend/notice_app/src/const/GlobalVar.js
+++ Ajou_Frontend/notice_app/src/const/GlobalVar.js
@@ -1,12 +1,7 @@
 
 const GlobalVar = {
     TITLE : "검색물고기",
-
-//    SERVER_URL : "http://localhost:8000",
-//    SERVER_URL : "http://192.168.1.1/react/test",
-    SERVER_URL : "http://localhost:8080",
-
-    API_URL : "http://localhost:8080",
+    API_URL : "http://118.67.135.137:8080",
 };
 
 export default GlobalVar;

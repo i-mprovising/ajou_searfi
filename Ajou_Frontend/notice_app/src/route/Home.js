500dd2f6207b781a201a80e44ab668a66d2b022c
jiyseo <jji5602@gmail.com>
2023-12-13 13:26:36 +0900

    Change Main URL

--- Ajou_Frontend/notice_app/src/route/Home.js
+++ Ajou_Frontend/notice_app/src/route/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Home() {
   return (
-    <iframe src="http://34.22.76.7:5601/goto/c08ba2d991952d9777331bd3ac9cbf26"
+    <iframe src="http://34.22.76.7:5601/goto/6b5edc2c6837fc52c406e17c63b6b675"
             title="main"
             style={{width:"100%", height:"100%", borderWidth:0 }}
     />
